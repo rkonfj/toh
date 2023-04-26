d2addcbb5af003b5ab4fa1059b32a1b1f662b491
rkonfj <rkonfj@gmail.com>
2023-04-26 14:13:57 +0800

    update traffic log format

--- cmd/socks5/server/stats.go
+++ cmd/socks5/server/stats.go
@@ -9,10 +9,10 @@ func logTrafficEvent(e *socks5.TrafficEvent) {
 	if e.In == 0 && e.Out == 0 {
 		return
 	}
-	logrus.WithField("stats.toh", e.DialerName).
-		WithField("stats.in_bytes", e.In).
-		WithField("stats.out_bytes", e.Out).
-		WithField("stats.in", e.LocalAddr).
-		WithField("stats.out", e.RemoteAddr).
+	logrus.WithField("stats_toh", e.DialerName).
+		WithField("stats_in_bytes", e.In).
+		WithField("stats_out_bytes", e.Out).
+		WithField("stats_in", e.LocalAddr).
+		WithField("stats_out", e.RemoteAddr).
 		Info()
 }
